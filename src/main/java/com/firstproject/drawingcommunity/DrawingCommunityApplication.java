16996cb06cadde39c540a396a757ffba00ecd926
HAEUN <heoh@solbirdplus.com>
2024-03-15 16:26:08 +0900

    First Commit 24-03-15 16:26

---
+++ src/main/java/com/firstproject/drawingcommunity/DrawingCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.firstproject.drawingcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrawingCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrawingCommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/firstproject/drawingcommunity/DrawingCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstproject.drawingcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrawingCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
