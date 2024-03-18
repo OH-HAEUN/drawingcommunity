a6f78c141f3c0e7008035d4ba4933eba8ff7d1f2
HAEUN <heoh@solbirdplus.com>
2024-03-18 16:09:57 +0900

    게시글 리스트, 상세페이지 24-03-18 16:09

--- src/main/java/com/firstproject/drawingcommunity/controller/BoardController.java
+++ src/main/java/com/firstproject/drawingcommunity/controller/BoardController.java
@@ -4,6 +4,7 @@ import com.firstproject.drawingcommunity.entity.Board;
 import com.firstproject.drawingcommunity.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +16,7 @@ public class BoardController {
     @Autowired
     private BoardService boardService;
 
-    @GetMapping("/boardwrite")
+    @GetMapping("/write")
     public String boardwrite() {
 
         return "/boards/boardwrite";
@@ -25,5 +26,17 @@ public class BoardController {
     public String boardWritePro(Board board) {
         boardService.write(board);
         return "";
+    }
+
+    @GetMapping("/list")
+    public String boardlist(Model model) {
+        model.addAttribute("list", boardService.boardList());
+        return "/boards/boardlist";
+    }
+
+    @GetMapping("/view")    // localhost:8080/board/view?id=1
+    public String boardView(Model model, Integer id) {
+        model.addAttribute("board", boardService.boardView(id));
+        return "/boards/boardview";
     }
 }
